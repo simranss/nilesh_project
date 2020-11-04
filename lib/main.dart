7a32c0373e0e3fd98a485cfe4f5d078ca1357607
Simran <simran.s.sharma00@gmail.com>
2020-11-04 09:06:52 +0530

    Included the login part in the main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 import 'package:flutter/material.dart';
-import 'package:nilesh_project/home%20page.dart';
+
+import 'package:nilesh_project/home page.dart';
 import 'contact.dart';
 import 'wishlist.dart';
 
@@ -36,6 +39,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  final auth = FirebaseAuth.instance;
   String name = "Simran", email = "simran@gmail.com";
   final GlobalKey<ScaffoldState> _key = new GlobalKey<ScaffoldState>();
   int _currentScreen = 0;
@@ -44,110 +48,157 @@ class _MyHomePageState extends State<MyHomePage> {
     Wishlist(),
     Contact(),
   ];
+  final _controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: _key, //In case you want to change the icon of drawer icon
 
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            UserAccountsDrawerHeader(accountName: Text(name), accountEmail: Text(email),
-              decoration: BoxDecoration(color: Colors.green),
+    final user = auth.currentUser;
+
+    if (user == null) {
+      return Scaffold(
+        body: Column(
+          children: [
+            Row(
+              children: [
+                Text('+91'),
+                TextField(
+                  controller: _controller,
+                )
+              ],
             ),
-            ListTile(
-              leading: Icon(Icons.home),
-              title: Text("Home"),
-              onTap: () {
-                Navigator.pop(context);
-                if(_currentScreen != 0) {
-                  setState(() {
-                    _currentScreen = 0;
-                  });
-                }
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.favorite),
-              title: Text("Wish list"),
-              onTap: () {
-                Navigator.pop(context);
-                if(_currentScreen != 1) {
-                  setState(() {
-                    _currentScreen = 1;
-                  });
-                }
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.email),
-              title: Text("Contact Us"),
-              onTap: () {
-                Navigator.pop(context);
-                if(_currentScreen != 2) {
-                  setState(() {
-                    _currentScreen = 2;
-                  });
-                }
-              },
-            ),
-            ListTile(
-              title: Text("Logout"),
-              onTap: () {
-                Navigator.pop(context);
-              },
+            RaisedButton(
+
             )
           ],
         ),
-      ),
+      );
+    } else {
+      return Scaffold(
+          key: _key, //In case you want to change the icon of drawer icon
 
-      body: Column(
-        children: <Widget>[
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: 0.20 * MediaQuery.of(context).size.height,
-            padding: EdgeInsets.only(left: 10.0, right: 20.0, top: 60.0),
-            alignment: Alignment.topCenter,
-            child: Row(
+          drawer: Drawer(
+            child: ListView(
+              padding: EdgeInsets.zero,
               children: <Widget>[
-                IconButton(
-                  icon: Icon(Icons.menu),
-                  color: Colors.black,
-                  onPressed: (){_key.currentState.openDrawer();}
+                UserAccountsDrawerHeader(
+                  accountName: Text(name), accountEmail: Text(email),
+                  decoration: BoxDecoration(color: Colors.green),
                 ),
-                Container(
-                  width: (0.75 * MediaQuery.of(context).size.width),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: "Search here",
-                      contentPadding: EdgeInsets.only(bottom: 0.0, left: 12.0, right: 12.0,),
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.grey, width: 0.5,),
-                        borderRadius: BorderRadius.circular(30.0),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.grey, width: 0.5,),
-                        borderRadius: BorderRadius.circular(30.0),
-                      ),
-                    ),
-                    style: TextStyle(color: Colors.black, fontSize: 18,),
-                    textAlign: TextAlign.center,
-                  ),
+                ListTile(
+                  leading: Icon(Icons.home),
+                  title: Text("Home"),
+                  onTap: () {
+                    Navigator.pop(context);
+                    if (_currentScreen != 0) {
+                      setState(() {
+                        _currentScreen = 0;
+                      });
+                    }
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.favorite),
+                  title: Text("Wish list"),
+                  onTap: () {
+                    Navigator.pop(context);
+                    if (_currentScreen != 1) {
+                      setState(() {
+                        _currentScreen = 1;
+                      });
+                    }
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.email),
+                  title: Text("Contact Us"),
+                  onTap: () {
+                    Navigator.pop(context);
+                    if (_currentScreen != 2) {
+                      setState(() {
+                        _currentScreen = 2;
+                      });
+                    }
+                  },
+                ),
+                ListTile(
+                  title: Text("Logout"),
+                  onTap: () {
+                    Navigator.pop(context);
+                    setState(() {
+                      auth.signOut();
+                    });
+                  },
                 )
               ],
             ),
           ),
-          Expanded(
-            child: Container(
-              width: MediaQuery.of(context).size.width,
-              padding: EdgeInsets.zero,
-              child: _drawerScreens[_currentScreen],
-            ),
+
+          body: Column(
+            children: <Widget>[
+              Container(
+                width: MediaQuery
+                    .of(context)
+                    .size
+                    .width,
+                height: 0.20 * MediaQuery
+                    .of(context)
+                    .size
+                    .height,
+                padding: EdgeInsets.only(left: 10.0, right: 20.0, top: 60.0),
+                alignment: Alignment.topCenter,
+                child: Row(
+                  children: <Widget>[
+                    IconButton(
+                        icon: Icon(Icons.menu),
+                        color: Colors.black,
+                        onPressed: () {
+                          _key.currentState.openDrawer();
+                        }
+                    ),
+                    Container(
+                      width: (0.75 * MediaQuery
+                          .of(context)
+                          .size
+                          .width),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: "Search here",
+                          contentPadding: EdgeInsets.only(
+                            bottom: 0.0, left: 12.0, right: 12.0,),
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.grey,
+                              width: 0.5,),
+                            borderRadius: BorderRadius.circular(30.0),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.grey,
+                              width: 0.5,),
+                            borderRadius: BorderRadius.circular(30.0),
+                          ),
+                        ),
+                        style: TextStyle(color: Colors.black, fontSize: 18,),
+                        textAlign: TextAlign.center,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  width: MediaQuery
+                      .of(context)
+                      .size
+                      .width,
+                  padding: EdgeInsets.zero,
+                  child: _drawerScreens[_currentScreen],
+                ),
+              )
+            ],
           )
-        ],
-      )
-    );
+      );
+    }
   }
+
+
 }
